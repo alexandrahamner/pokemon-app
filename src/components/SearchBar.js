ae552bc817444ecd583e4619e8f861e1ad10b8e7
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-12-31 18:13:11 -0600

    feat: finished navbar.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,22 +6,24 @@ export default function SearchBar(props) {
 
     
     return (
-    <div className="search-container">
-        <form>
-            <div className="field ui icon input">
-                <label></label>
-                <input 
-                    placeholder="Search For Pokemon"
-                    type="text"
-                    // value={}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-                <i onClick={(e) => {e.preventDefault(); props.getPokemon(search)}} className="inverted circular search link icon"></i>
-            </div>
-            <button className="ui icon button" onClick={(e) => {e.preventDefault(); props.getFavPokemon()}} ><i className="filter-favs icon heart"></i></button>
-            <button className="ui button" onClick={(e) => window.location.reload}>All</button>
-        </form>
-        
+    <div className="search-container ui top fixed menu mini">
+        <div className="item">Pokemon App</div>
+        <p className="ui icon button item" onClick={(e) => {e.preventDefault(); props.getFavPokemon()}} ><i className="filter-favs icon heart"></i></p>
+        <p className="ui button item" onClick={(e) => window.location.reload}>All</p>
+        <div className="right menu">
+            <div className="item">
+                <div className="field ui icon input">
+                    <label></label>
+                    <input 
+                        placeholder="Search For Pokemon"
+                        type="text"
+                        // value={}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <i onClick={(e) => {e.preventDefault(); props.getPokemon(search)}} className="search link icon"></i>
+                </div>
+        </div>
+        </div>
     </div>
     );
     
